31db4627087df952659363a670df453d73db744f
TAlex <ta-lex@mail.ru>
2017-02-27 20:42:33 +0300

    Deals: Migrations

---
+++ database/migrations/2017_02_27_171927_create_deals_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDealsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('deals', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->date('date');
+            $table->enum('status', [
+                'New',
+                'Assigned',
+                'In Progress',
+                'Converted',
+                'Recycled'
+            ]);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('deals');
+    }
+}
